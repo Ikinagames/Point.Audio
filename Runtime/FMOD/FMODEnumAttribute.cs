b2aaff9819707a71e7475e51a84e6a7605bc8f48
Syadeu <syadeu@gmail.com>
2022-03-07 20:37:31 +0900

    no message

--- Runtime/FMOD/FMODEnumAttribute.cs
+++ Runtime/FMOD/FMODEnumAttribute.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2021 Ikina Games
+﻿// Copyright 2022 Ikina Games
 // Author : Seung Ha Kim (Syadeu)
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,6 @@
 #if (UNITY_EDITOR || DEVELOPMENT_BUILD) && !POINT_DISABLE_CHECKS
 #define DEBUG_MODE
 #endif
-
 
 namespace Point.Audio
 {
